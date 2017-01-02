cb71a611c5194296a1910e79956ef080901355ea
Steven Harradine <stevenharradine@gmail.com>
2017-01-01 22:25:07 -0500

    list to size

--- build.sh
+++ build.sh
@@ -5,18 +5,18 @@ root=$1
 folder=$2
 path=$root/$folder
 md5_path=$current_working_directory/$folder.md5
-list_path=$current_working_directory/$folder.list
+size_path=$current_working_directory/$folder.size
 
 cd $path
 
 # reset logs
 echo -n "" > $md5_path
-echo -n "" > $list_path
+echo -n "" > $size_path
 
 for line in $(find ./); do 
 	if [ -f "${line}" ] ; then
-		ls -lash $line >> $list_path
 		md5sum $line >> $md5_path
+		ls --all --size --human-readable $line >> $size_path
 	fi
 done
 
